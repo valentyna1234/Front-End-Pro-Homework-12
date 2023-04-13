78a7e10ba0010a65c529712f4dda68729517e64f
valentyna1234 <pisareva.valya@gmail.com>
2023-04-13 12:43:35 +0300

    Add changed index.js file

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 //Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
-function removeElement() {
-  let array = [1, 2, 3, 4, 5, 6, 7];
-  const item = 5;
+function removeElement(array, item) {
+
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] % item != 0) {
+    if (array[i] / item != 1) {
       newArray.push(array[i]);
     }
   }
   console.log(newArray);
 }
-removeElement()
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+const item = 4;
+
+removeElement(array, item)
